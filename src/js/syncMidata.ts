89be46f95064a7a0c46353797d48a9ef583966e6
Lela810 <leandro.klaus03@gmail.com>
2025-03-04 16:03:17 +0000

    Mar 4, 2025, 5:03 PM

--- src/js/syncMidata.ts
+++ src/js/syncMidata.ts
@@ -1,7 +1,8 @@
-import http from 'node:http';
+import https from 'node:http';
 
 export function downloadMidataRecipients() {
-	http
+	let parsedData;
+	https
 		.get('https://db.scout.ch/groups/6513/people.json', (res) => {
 			res.headers['X-TOKEN'] = process.env.MIDATA_API_TOKEN;
 			res.setEncoding('utf8');
@@ -10,11 +11,12 @@ export function downloadMidataRecipients() {
 				rawData += chunk;
 			});
 			res.on('end', () => {
-				const parsedData = JSON.parse(rawData);
+				parsedData = JSON.parse(rawData);
 				console.log(parsedData);
 			});
 		})
 		.on('error', (e) => {
 			console.error(`Got error: ${e.message}`);
 		});
+	return parsedData;
 }
