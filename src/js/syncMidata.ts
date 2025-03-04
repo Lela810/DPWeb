ab07644bb86fa6eb34ca0b68bf06dbcb329a29ca
Lela810 <leandro.klaus03@gmail.com>
2025-03-04 20:33:17 +0000

    Mar 4, 2025, 9:33 PM

--- src/js/syncMidata.ts
+++ src/js/syncMidata.ts
@@ -1,29 +1,34 @@
 import https from 'node:https';
 import { MiDataPerson } from '../types/MiDataPerson';
 
-export function downloadMidataRecipients() {
-	const options = {
-		hostname: 'db.scout.ch',
-		path: '/groups/6513/people.json',
-		headers: {
-			'X-TOKEN': process.env.MIDATA_API_TOKEN,
-		},
-	};
+export function downloadMidataRecipients(): Promise<MiDataPerson[]> {
+	return new Promise((resolve, reject) => {
+		const options = {
+			hostname: 'db.scout.ch',
+			path: '/groups/6513/people.json',
+			headers: {
+				'X-TOKEN': process.env.MIDATA_API_TOKEN,
+			},
+		};
 
-	https
-		.get(options, (res) => {
-			res.setEncoding('utf8');
-			let rawData = '';
-			res.on('data', (chunk) => {
-				rawData += chunk;
+		https
+			.get(options, (res) => {
+				res.setEncoding('utf8');
+				let rawData = '';
+				res.on('data', (chunk) => {
+					rawData += chunk;
+				});
+				res.on('end', () => {
+					try {
+						const parsedData: MiDataPerson[] = JSON.parse(rawData);
+						resolve(parsedData);
+					} catch (error) {
+						reject(error);
+					}
+				});
+			})
+			.on('error', (e) => {
+				reject(e);
 			});
-			res.on('end', () => {
-				const parsedData: MiDataPerson = JSON.parse(rawData);
-				return parsedData;
-				//console.log(parsedData);
-			});
-		})
-		.on('error', (e) => {
-			console.error(`Got error: ${e.message}`);
-		});
+	});
 }
