f96bb77b1d98b8b72e31fae1d82241894cea4177
Lela810 <leandro.klaus03@gmail.com>
2025-03-04 15:18:36 +0000

    Mar 4, 2025, 4:18 PM

--- src/public/js/confirmSave.js
+++ src/public/js/confirmSave.js
@@ -5,3 +5,11 @@ function confirmSave() {
 		button.style = '';
 	}, 2000);
 }
+
+function confirmSync() {
+	const button = document.getElementById('sync');
+	button.style.backgroundColor = 'lightgreen';
+	setTimeout(() => {
+		button.style = '';
+	}, 2000);
+}
